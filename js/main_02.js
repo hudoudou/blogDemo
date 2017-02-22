7bcd7d3ac05b2f6532013cb4ea1822e6ca44463a
hudoudou <529325329@qq.com>
2017-02-23 02:15:57 +0800

    第一次添加

---
+++ js/main_02.js
@@ -0,0 +1,25 @@
+
+
+function bt() 
+{
+	var ycxs=document.getElementById("ycxs_id");
+	var tsnr=document.getElementById("tsnr_id");
+	var ycxs_top=document.getElementById("ycxs");
+	var tsnr_top=document.getElementById("ts");
+	ycxs.className="yincang";
+	tsnr.className="ycxs_c xiabianguang";
+	ycxs_top.className="yangshi1 shouxing";
+	tsnr_top.className="xiabianguang yangshi1 shouxing beijing"
+}
+
+function bt1()
+{
+	var ycxs=document.getElementById("ycxs_id");
+	var tsnr=document.getElementById("tsnr_id");
+	var ycxs_top=document.getElementById("ycxs");
+	var tsnr_top=document.getElementById("ts");
+	ycxs.className="ycxs_c";
+	tsnr.className="yincang";
+	ycxs_top.className="xiabianguang yangshi1 shouxing beijing";
+	tsnr_top.className="yangshi1 shouxing"	
+}
